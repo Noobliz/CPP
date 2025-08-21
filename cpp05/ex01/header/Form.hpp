c292c765b19d368efce9a3d821feeca562bc9cf4
lisux <lisetuieg@gmail.com>
2025-08-21 15:25:12 +0200

    main test

--- cpp05/ex01/header/Form.hpp
+++ cpp05/ex01/header/Form.hpp
@@ -31,3 +31,5 @@ class Form
 
         void beSigned(const Bureaucrat &x);
 };
+
+//surcharge <<
--- cpp05/ex01/src/main.cpp
+++ cpp05/ex01/src/main.cpp
@@ -3,48 +3,8 @@
 
 int main()
 {
-    std::cout<<"\n|-----------[Grade too low]-----------|"<<std::endl;
-    try
-    {
-        Bureaucrat bob("Bob", 15000);
-    }
-    catch (Bureaucrat::GradeTooHighException &e) 
-    {
-        std::cerr << e.what() << '\n';
-        //problems
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    
-    std::cout<<"\n|-----------[Grade too high for promotion]-----------|"<<std::endl;
-    Bureaucrat ernest("Ernest", 1);
-    std::cout << ernest<<std::endl;
-    try
-    {
-        ernest.promotion();
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr<<e.what()<<std::endl;
-    }
-    std::cout <<"after promotion attempted: "<< ernest<<std::endl;
-
-    std::cout<<"\n|-----------[Assignment operator]-----------|"<<std::endl;
-    Bureaucrat alex("Alex", 5);
-    std::cout<<alex<<std::endl;
-    std::cout<<"before assignment "<<ernest<<std::endl;
-    ernest = alex;
-    std::cout<<"after assignment "<<ernest<<std::endl;
-    
-    
-    
-    std::cout<<"\n|-----------[Copy constructor]-----------|"<<std::endl;
-    Bureaucrat copy(ernest);
-    std::cout<<copy<<std::endl;
-    
-     std::cout<<"\n|-----------[SIGNING FORM]-----------|"<<std::endl;
+    std::cout<<"\n|-----------[SIGNING FORM]-----------|"<<std::endl;
+    Bureaucrat alex("Alex", 3);
     Form ab("AB11", 5, 2);
     alex.signForm(ab);
 
