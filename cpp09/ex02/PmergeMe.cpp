2f7f3ae3e7f7fcc791759db16ba551397d1760f4
lisux <lisetuieg@gmail.com>
2025-10-24 18:55:25 +0200

    pb with insertion vect fixed

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -29,6 +29,11 @@ bool PmergeMe::storeNb(char **argv, int argc)
         _vector.push_back(num);
         _deque.push_back(num);
     }
+    if (_vector.size() < 2)
+    {
+        //print smthing ?
+        return false;
+    }
     for (size_t i = 0; i < _vector.size(); ++i)
         std::cout<<_vector[i]<<" ";
     std::cout<<std::endl;
@@ -37,8 +42,10 @@ bool PmergeMe::storeNb(char **argv, int argc)
 
 void PmergeMe::sortVector(std::vector<int> &v)
 {
-    if (v.size() == 2)
+    if (v.size() <= 2)
     {
+        if (v.size() <= 1)
+            return ;
         if (v[0] > v[1])
             std::swap(v[0], v[1]);
         return ;
@@ -66,6 +73,7 @@ void PmergeMe::sortVector(std::vector<int> &v)
             ++i;
         }
     }
+    
     sortVector(big);
 
     std::cout<<"big: ";   
@@ -82,31 +90,33 @@ void PmergeMe::sortVector(std::vector<int> &v)
 
 
     std::vector<int> indexToInsert;
+    for (size_t i = 0; i < _JacobSuite.size(); ++i)
+    {
+        int index = _JacobSuite[i];
+        if (index < static_cast<int>(small.size()))
+            indexToInsert.push_back(index);
+    }
+
+
     for (size_t i = 0; i < small.size(); ++i)
     {
-        int index = i;
-        if (i < _JacobSuite.size() && index == _JacobSuite[i])
-            indexToInsert.push_back(_JacobSuite[i]);
+        std::vector<int>::iterator it;
+        int idx = i;
+        it = std::find(indexToInsert.begin(), indexToInsert.end(), idx);
+        if (it == indexToInsert.end())
+            indexToInsert.push_back(idx);
     }
 
     std::cout<<"IndexToInsert: ";   
     for (size_t i = 0; i < indexToInsert.size(); ++i)
         std::cout<<indexToInsert[i]<<" ";
     std::cout<<std::endl;
-
-    for (size_t i = 0; i < small.size(); ++i)
-    {
-        std::vector<int>::iterator it;
-        it = std::find(_JacobSuite.begin(), _JacobSuite.end(), i);
-        if (it == _JacobSuite.end())
-            indexToInsert.push_back(i);
-    }
     //=================================================|INSERT|============================================
 
     for (size_t i = 0; i < small.size(); ++i)
     {
-        std::vector<int>::iterator it = std::lower_bound(big.begin(), big.end(), small[_JacobSuite[i]]);
-        big.insert(it, small[_JacobSuite[i]]);
+        std::vector<int>::iterator it = std::lower_bound(big.begin(), big.end(), small[indexToInsert[i]]);
+        big.insert(it, small[indexToInsert[i]]);
     }
     // std::cout<<"big after insertion: ";   
     // for (size_t i = 0; i < big.size(); ++i)
